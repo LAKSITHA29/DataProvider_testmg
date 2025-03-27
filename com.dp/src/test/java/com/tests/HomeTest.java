44e1dcc82dc432dccc3808378f07237ff18ccc54
LAKSITHA29 <laksitharavi@gmail.com>
2025-03-27 12:01:10 +0530

    first commit

---
+++ com.dp/src/test/java/com/tests/HomeTest.java
@@ -0,0 +1,17 @@
+package com.tests;
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class HomeTest extends BaseTest {
+	com.pages.HomePage objHome;
+	 @Test(priority = 0)
+	  public void loginTest() {
+		  objHome=new com.pages.HomePage(driver);
+		  String loginPageTitle=objHome.getLoginPageText();
+		  //Assert.assertTrue(loginPageTitle.contains("Welcome to the new Tricentis store!"));
+	  }
+
+}
